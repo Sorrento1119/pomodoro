2cd8551608bf0fedb777c6931ba12adf5a06b051
sorrento1119 <sablethar@gmail.com>
2026-01-16 12:00:04 +0530

    added customization

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-const bells = new Audio("./sounds/bell.wav");
+const bells = new Audio("./bell.wav");
 const startBtn = document.querySelector(".btn-start");
+const resetBtn = document.querySelector(".btn-reset");
+const sessionInput = document.querySelector("#session-input");
 const session = document.querySelector(".minutes");
 let myInterval;
 let state = true;
 
 const appTimer = () => {
-  const sessionAmount = Number.parseInt(session.textContent);
+  const sessionAmount = Number.parseInt(sessionInput.value);
 
   if (state) {
     state = false;
+    sessionInput.disabled = true;
+    session.textContent = sessionAmount;
+    document.querySelector(".seconds").textContent = "00";
     let totalSeconds = sessionAmount * 60;
 
-   const updateSeconds = () => {
-  const minuteDiv = document.querySelector(".minutes");
-  const secondDiv = document.querySelector(".seconds");
+    const updateSeconds = () => {
+      const minuteDiv = document.querySelector(".minutes");
+      const secondDiv = document.querySelector(".seconds");
 
-  totalSeconds--;
+      totalSeconds--;
 
-  let minutesLeft = Math.floor(totalSeconds / 60);
-  let secondsLeft = totalSeconds % 60;
+      let minutesLeft = Math.floor(totalSeconds / 60);
+      let secondsLeft = totalSeconds % 60;
 
-  if (secondsLeft < 10) {
-    secondDiv.textContent = "0" + secondsLeft;
-  } else {
-    secondDiv.textContent = secondsLeft;
-  }
-  minuteDiv.textContent = `${minutesLeft}`;
+      if (secondsLeft < 10) {
+        secondDiv.textContent = "0" + secondsLeft;
+      } else {
+        secondDiv.textContent = secondsLeft;
+      }
+      minuteDiv.textContent = `${minutesLeft}`;
 
-  if (minutesLeft === 0 && secondsLeft === 0) {
-    bells.play();
-    clearInterval(myInterval);
-    
-  }
+      if (minutesLeft === 0 && secondsLeft === 0) {
+        bells.play();
+        clearInterval(myInterval);
+        state = true;
+        sessionInput.disabled = false;
+      }
     };
-    
+
     myInterval = setInterval(updateSeconds, 1000);
   } else {
     alert("Session has already started.");
   }
 };
+
+const resetTimer = () => {
+  clearInterval(myInterval);
+  state = true;
+  sessionInput.disabled = false;
+  session.textContent = sessionInput.value;
+  document.querySelector(".seconds").textContent = "00";
+};
+
 startBtn.addEventListener("click", appTimer);
+resetBtn.addEventListener("click", resetTimer);
